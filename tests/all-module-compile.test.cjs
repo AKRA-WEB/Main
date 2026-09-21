f64c89fd69f19f58f820bd951f801aec79d4a3a6
AKRA Web Dev <akra.web.dev@gmail.com>
2026-09-21 16:36:16 +0700

    Hide embedded sidebar on desktop

--- tests/all-module-compile.test.cjs
+++ tests/all-module-compile.test.cjs
@@ -7,9 +7,9 @@ const vm=require('node:vm');
 const root=path.resolve(__dirname,'../..');
 const modules=['Main','PO','PR','GR','Returnitem','KPITracker','Picking','TRDAKRA','AKRA','SOP','Evaluation form'];
 const expectedVersions={
- Main:'20260921.05', PO:'20260919.04', PR:'20260921.03', GR:'20260920.04',
- Returnitem:'20260919.03', KPITracker:'20260921.03', Picking:'20260918.04',
- TRDAKRA:'20260920.03', AKRA:'20260919.05', SOP:'20260919.05', 'Evaluation form':'20260918.05'
+ Main:'20260921.06', PO:'20260919.05', PR:'20260921.04', GR:'20260920.05',
+ Returnitem:'20260919.04', KPITracker:'20260921.04', Picking:'20260918.05',
+ TRDAKRA:'20260920.04', AKRA:'20260919.06', SOP:'20260919.06', 'Evaluation form':'20260918.06'
 };
 for(const name of modules)test(`${name}: all entry scripts compile and version files agree`,()=>{
  const dir=path.join(root,name);
